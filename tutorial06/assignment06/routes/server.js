b06959e5b664e4faf3588faac22e6b25c974e9b5
yuhao <wang.yuhao@campus.lmu.de>
2018-12-03 02:09:40 +0100

    fetch API

--- tutorial06/assignment06/routes/server.js
+++ tutorial06/assignment06/routes/server.js
@@ -1,19 +1,30 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var fs=require("fs");
 var imagedatabase = require('./imagedatabase');
 
 var app = express();
-app.use(express.static("./"));
-app.use(bodyParser.json);
+// app.use(express.static("../"));
+// app.use(express.static(__dirname + '/public'));
+app.use(express.static('../public'));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 
 
-app.get("../routes/server.js", function(request, response){
-    console.log(imagedatabase);      // this a json object
+app.post("/server.js", function(request, response){
+    // console.log(request.body);      // this a json object
+    console.log("/server.js start");
+    console.log(imagedatabase[1].url); 
     response.send(JSON.stringify(imagedatabase));    // echo the result back
   });
   
+  app.get("/routes/imagedatabase.json", function(request, response){
+    console.log(imagedatabase);      // this a json object
+    print(imagedatabase);
+    response.send(JSON.stringify(imagedatabase));    // echo the result back
+  });
+
   app.listen(3000, function() {   //监听http://127.0.0.1:3000端口
       console.log("server start");
   });
